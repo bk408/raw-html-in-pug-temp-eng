f9245680d48d360142cdcf3bf4a423c361242d11
bk408 <bhavyakhatri1912@gmail.com>
2022-12-04 20:59:13 +0530

    add write file

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");       // first we import the express module
 const path = require("path");
+const fs = require("fs");
 const app = express();           // create an app 
 const port = 80;                // run on port on 80
 
 
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static'));  // for Serving static files
+app.use(express.urlencoded())
 
 // PUG SPECIFIC STUFF
 app.set('view engine', 'pug')   // Set the template engine as pug
@@ -19,6 +21,18 @@ app.get('/', (req, res)=>{
     const params = {'title': 'pug is the best template engine', "content": con};
     res.status(200).render('index.pug', params);
 });
+
+app.post('/',(req, res)=>{
+    name = req.body.name
+    age = req.body.age
+    gender = req.body.gender
+    address = req.body.address
+    more = req.body.more
+    let outputTOWRITE = `The name of client is ${name}, ${age} years old, ${gender}, residing at ${address}. More about him/her: ${more} `
+    fs.writeFileSync('output.txt',outputTOWRITE )
+    const params = {'meassage': 'Your form has been submitted sucessfully',};
+    res.status(200).render('index.pug', params);
+})
 
 
 // START THE SERVER
